9e17406a2258922e2b4510527a19d2cb21778f50
CarlosLopxz <carloslxpxz@gmail.com>
2024-09-01 17:41:41 -0500

    ee

---
+++ Views/Copyright/copyright.php
@@ -0,0 +1,3 @@
+<div class="copyright">
+        &copy;<a href="">©Copyright </a> Todos los derechos reservados. La reproducción, distribución o transmisión de cualquier parte de este sitio sin el permiso expreso por escrito de SigmaSoft está prohibida.
+</div>
